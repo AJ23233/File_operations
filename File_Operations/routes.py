a20f5ee1fb7dcf4c20475750ecc81a7d49bf06ea
Ajay Vanara <a23vanara@gmail.com>
2022-06-11 18:39:35 +0530

    chg: added fetch data routes

--- File_Operations/routes.py
+++ File_Operations/routes.py
@@ -1,8 +1,36 @@
+# -*- coding: utf-8 -*-
+# Filename: routes.py
+# Description: This file contains all the application routes
+# Author: Ajay Vanara
 
+# Built-in imports 
 import connexion
+from flask import request, jsonify
+
+# Application imports
+from operations.document_ops import DocOps
+
 
 app = connexion.App(__name__, specification_dir="./")
 
-@app.route("/ping")
+
+@app.route("/v1/ping")
 def ping():
-	return "pong"
+	"""
+	this method is used for health check of the application
+	imput: None
+	output: json
+	"""
+	return jsonify({"data":"Pong"})
+
+
+@app.route("/v1/fetch_data")
+def fetch_details():
+	"""
+	This method is used for search operations to be performed on the file
+	input: rows - Numbers of rows to be fetched from file 
+	output: fetched response (json)   
+	"""
+	rows = request.args.get('rows')
+	res = DocOps().fetch_data(rows)
+	return res
